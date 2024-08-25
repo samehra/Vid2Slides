d9b8e82860721ede6a34eee70c19572adca0db38
Saurabh Mehra <samehra@vt.edu>
2024-08-24 20:26:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,22 +11,23 @@ def main(args):
   # Step 3: Preview the captured slides
   preview_images(args.output_folder, image_count=args.preview_count)
 
-  if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Capture slides from a YouTube video.")
-    
-    # Add the video URL argument
-    parser.add_argument('video_url', type=str, help="URL of the YouTube video")
-    
-    # Optional arguments with default values
-    parser.add_argument('--ssim_threshold', type=float, default=0.9, help="SSIM threshold for slide detection")
-    parser.add_argument('--histogram_threshold', type=float, default=0.7, help="Histogram threshold for slide detection")
-    parser.add_argument('--time_gap', type=int, default=50, help="Time gap between frames to compare")
-    parser.add_argument('--max_frame_count', type=int, default=10000, help="Maximum number of slides to capture")
-    parser.add_argument('--output_folder', type=str, default='slides', help="Folder to save captured slides")
-    parser.add_argument('--verbose', type=bool, default='False', help="Print the count of slides and % of frames parsed")
-    parser.add_argument('--preview_count', type=int, default=100, help="Number of slides to preview")
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Capture slides from a YouTube video.")
+  
+  # Add the video URL argument
+  parser.add_argument('video_url', type=str, help="URL of the YouTube video")
+  
+  # Optional arguments with default values
+  parser.add_argument('--ssim_threshold', type=float, default=0.9, help="SSIM threshold for slide detection")
+  parser.add_argument('--histogram_threshold', type=float, default=0.7, help="Histogram threshold for slide detection")
+  parser.add_argument('--time_gap', type=int, default=50, help="Time gap between frames to compare")
+  parser.add_argument('--max_frame_count', type=int, default=10000, help="Maximum number of slides to capture")
+  parser.add_argument('--output_folder', type=str, default='slides', help="Folder to save captured slides")
+  # action='store_true' is used for verbose, which means if you include --verbose in the command, args.verbose will be True; otherwise, it will be False.
+  parser.add_argument('--verbose', action='store_true', help="Print the count of slides and % of frames parsed")
+  parser.add_argument('--preview_count', type=int, default=100, help="Number of slides to preview")
 
-    args = parser.parse_args()
+  args = parser.parse_args()
 
-    # Call the main function with parsed arguments
-    main(args)
+  # Call the main function with parsed arguments
+  main(args)
