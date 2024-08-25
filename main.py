4db1a4d058620b68114ed12f194d05f1156353b9
Saurabh Mehra <samehra@vt.edu>
2024-08-24 18:34:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from your_script_name import download_video, capture_slides, preview_images
+from code import download_video, capture_slides, preview_images
 
 def main(url):
   # Step 1: Download the video
   video_path = download_video(url)
   
-  # Step 2: Capture slides from the video
-  capture_slides(video_path, verbose=True)
+  # Step 2: Capture slides from the video in the output_folder
+  capture_slides(video_path, output_folder='slides', max_frame_count = 10000, ssim_threshold=0.9, histogram_threshold=0.7, time_gap=50, verbose=True)
   
   # Step 3: Preview the captured slides
-  preview_images('slides', image_count=100)
+  max_slides = 100
+  preview_images('slides', image_count=max_slides)
